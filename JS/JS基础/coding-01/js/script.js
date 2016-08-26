ef8e3809eded2b428caaa802d1babf84afdde9ab
tanglili <cyj@2016>
2016-08-26 13:58:39 +0800

    增加了验证码倒计时的js

--- JS/JS基础/coding-01/js/script.js
+++ JS/JS基础/coding-01/js/script.js
@@ -77,3 +77,26 @@ function backTop(btnId) {
 }
 backTop('gotop-btn');
 
+/*
+ * js获取验证码倒计时效果
+ */
+var wait = 60;
+function time(o) {
+    if (wait == 0) {
+        o.removeAttribute("disabled");
+        o.value = "获取验证码";
+        wait = 60;
+    } else {
+        o.setAttribute("disabled", true);
+        o.value = "重新发送(" + wait + ")";
+        wait--;
+        setTimeout(function () {
+                time(o)
+            },
+            1000)
+    }
+}
+document.querySelector(".codes-btn").onclick = function () {
+    time(this);
+};
+
